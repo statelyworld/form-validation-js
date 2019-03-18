591765cad3463fd0fa370e50eba8515331b63ea0
Stately World <statelyworld@gmail.com>
2019-03-18 22:20:10 +0530

    Update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# form-validation-js
+# Form Validation (Wordpress Plugin)
 Wordpress plugin for validation in input field number do not enter +91 and 0 in start.
--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <?php
 /**
- * @package Hello_Dolly
- * @version 1.6
+ * @package Form_Validation_Js
+ * @version 1.0
  */
 /*
 Plugin Name: Form Validation JS
-Plugin URI: http://gauravrjoshi.com/plugins/
+Plugin URI: http://statelyworld.com/plugins
 Description:This is a plugin for wordpress using for input feild telephone.Use class 'validaton' in input form telepone.
 Author: Gaurav Joshi
 Version: 1.0
-Author URI: http://gauravrjoshi.com/
+Author URI: http://statelyworld.com
 Text Domain: form-validation-js
 */
 add_action('init', 'form_validation_js');
+
 function form_validation_js() 
 {
 	$src = plugins_url('form-validation-js.js', __FILE__);
-    wp_register_script( 'form-validation-js', $src ,array(),false,true);
-	wp_enqueue_script( 'form-validation-js' );
+    wp_register_script('form-validation-js', $src ,array(),false,true);
+	wp_enqueue_script('form-validation-js');
 }
-
-
